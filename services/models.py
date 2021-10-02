9576cdd9783a5e54a5917264732dd8a06651bb60
Dishank Tak <39084848+Dishank99@users.noreply.github.com>
2021-10-02 17:59:01 +0530

    implemented initial db structure

--- services/models.py
+++ services/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from users.models import *
 
-# Create your models here.
+class ServiceLanguage(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Content(models.Model):
+    title = models.CharField(max_length=100)
+    video_url = models.URLField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Service(models.Model):
+    categories = models.ForeignKey(DoctorCategory, on_delete=models.SET_NULL, null=True, blank=True)
+    contents = models.ManyToManyField(Content)
+    language = models.ForeignKey(ServiceLanguage, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.categories.name
--- users/models.py
+++ users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class SalesOfficerRegion(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class SalesOfficer(models.Model):
+    name = models.CharField(max_length=50)
+    region = models.ForeignKey(SalesOfficerRegion, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.region}'
+
+class DoctorCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    sales_officer = models.ForeignKey(SalesOfficer,on_delete=models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(DoctorCategory, on_delete=models.SET_NULL, null=True, blank=True, )
+    
+    def __str__(self):
+        return self.name

4cecb910630444a26847ed221933bad14a05a440
Dishank Tak <39084848+Dishank99@users.noreply.github.com>
2021-10-24 01:41:07 +0530

    implemented version1

--- services/models.py
+++ services/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from users.models import *
+from django.db.models.signals import post_save
+from .signals import post_save_activities_for_language_save
 
 class ServiceLanguage(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
@@ -10,7 +12,9 @@ class ServiceLanguage(models.Model):
 class Content(models.Model):
     title = models.CharField(max_length=100)
     video_url = models.URLField(null=True, blank=True)
+    video_thumnail = models.ImageField(upload_to='media/thumbnails', null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    video_embed_code = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -21,4 +25,6 @@ class Service(models.Model):
     language = models.ForeignKey(ServiceLanguage, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return self.categories.name
+        return f'{self.categories.name} ({self.language.name})'
+
+post_save.connect(post_save_activities_for_language_save, sender=ServiceLanguage)
---
+++ users/services.py
@@ -0,0 +1,15 @@
+import qrcode
+from django.conf import settings
+import os
+
+LINK_TEMPLATE_FOR_QRCODE = 'http://127.0.0.1:8000/{}'
+
+def generate_qrcode_for_doctor(doctor):
+    print('reached here services')
+    qr_code = qrcode.make(LINK_TEMPLATE_FOR_QRCODE.format(doctor.id))
+    image_dest = f'qr_codes\\{doctor.id}_{doctor.name}_{doctor.sales_officer.region}.jpg'
+    storage_path = os.path.join(settings.MEDIA_ROOT_URL, image_dest)
+    qr_code.save(storage_path)
+    print(storage_path)
+    return image_dest
+    

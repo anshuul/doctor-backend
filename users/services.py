9220b0369c31029aa61f4b83c53456009d377581
anshuul <anshultated1999@gmail.com>
2021-10-28 01:57:38 +0530

    url changed in users/services.py

--- users/services.py
+++ users/services.py
@@ -2,7 +2,7 @@ import qrcode
 from django.conf import settings
 import os
 
-LINK_TEMPLATE_FOR_QRCODE = 'http://127.0.0.1:8000/{}'
+LINK_TEMPLATE_FOR_QRCODE = 'https://gastroawareness.online/{}'
 
 def generate_qrcode_for_doctor(doctor):
     print('reached here services')
